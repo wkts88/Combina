213a425cb337284c2d328e3e9ac1f97fe860e9d7
wkts89 <wkts89@gmail.com>
2014-03-27 19:12:57 +0100

    commit en nube

---
+++ P9ESO/src/com/me/p9eso/game/WorldController.java
@@ -0,0 +1,11 @@
+package com.me.p9eso.game;
+
+import com.me.p9eso.P9ESO;
+
+public class WorldController {
+	private static final String TAG = WorldController.class.getName();
+	
+	public WorldController(){}
+	public void init(){}
+	public void update (float deltaTime){}
+}
---
+++ P9ESO/src/com/me/p9eso/util/Constants.java
@@ -0,0 +1,5 @@
+package com.me.p9eso.util;
+
+public class Constants {
+	
+}
